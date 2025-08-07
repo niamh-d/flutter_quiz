7810117d4daf7a230162efabab21705ea67fa3a2
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-08-07 18:09:28 +0300

    styling with google fonts

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 import 'package:flutter_quiz/answer_button.dart';
 import 'package:flutter_quiz/data/questions.dart';
 
@@ -34,8 +36,10 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           children: [
             Text(
               currentQuestion.text,
-              style: TextStyle(
-                color: Colors.white,
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 201, 153, 251),
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
               ),
               textAlign: TextAlign.center,
             ),
