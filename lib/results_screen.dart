718a8ee25055cc350322f3217b75ef9b8307b6ce
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-08-09 12:28:45 +0300

    calculating num of qs answered correctly

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -24,6 +24,14 @@ class ResultsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectAnswers = summaryData
+        .where(
+          (q) => q['selected_answer'] == q['correct_answer'],
+        )
+        .length;
+
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -31,9 +39,11 @@ class ResultsScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You answered X out of Y questions correctly'),
+            Text(
+              'You answered $numCorrectAnswers out of $numTotalQuestions questions correctly',
+            ),
             const SizedBox(height: 30),
-            QuestionsSummary(getSummaryData()),
+            QuestionsSummary(summaryData),
             const SizedBox(height: 30),
             TextButton(onPressed: () {}, child: const Text('Resart Quiz')),
           ],
